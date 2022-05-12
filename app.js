48318a45c2960fd7888d4061a5aeff1fc66b77c5
Vasantha Kumar <105373633+vasanthakumar1075@users.noreply.github.com>
2022-05-12 13:09:26 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+
+
+        function scrollWin() 
+        {
+          window.scrollTo({
+            top: 10,
+            behavior: 'smooth',
+          })
+        }
+        function scrollWin1() 
+        {
+          window.scrollTo({
+            top: 650,
+            behavior: 'smooth',
+          })
+        }
+        function scrollWin2() 
+        {
+          window.scrollTo({
+            top: 1050,
+            behavior: 'smooth',
+          })
+        }
+        function scrollWin3() 
+        {
+          window.scrollTo({
+            top: 1650,
+            behavior: 'smooth',
+          })
+        }
+        
